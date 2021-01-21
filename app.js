9877c899bf1a3fde3de4156d2af79666cbc72415
Christopher Ryan Limoa <clim0061@student.monash.edu>
2021-01-21 14:34:08 +1100

    Add pixel colour changing behaviour

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ let container = document.getElementById("container");
 for (let i =0; i<16; i++){
     for (let j =0; j<16; j++){
         let div = document.createElement('div');
-        // div.setAttribute('class', 'pixel');
-        
+        div.setAttribute('class', 'pixel-unselected');
+        div.setAttribute('onmouseover', "this.className='pixel-selected'");
         container.appendChild(div);
     }
 }
